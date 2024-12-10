bff3691487873c91e47029af700644391bb7a0ab
Yogesh Goyal <yogeshgoyal1989@gmail.com>
2024-12-10 10:55:35 -0500

    Changin logo

--- components/banner.js
+++ components/banner.js
@@ -2,7 +2,7 @@ const Banner = () => {
     return (
         <header>
             <div>
-                <img src="./globe.svg" alt="logo" />
+                <img src="./GloboLogo.png" alt="logo" />
             </div>
             <div>
             Providing houses all over the world 
