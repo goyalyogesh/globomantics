8ecb211a390915b27e39d7d5efbdf86082b5394b
Yogesh Goyal <yogeshgoyal1989@gmail.com>
2024-12-10 10:30:28 -0500

    initial commit

---
+++ components/banner.js
@@ -0,0 +1,14 @@
+const Banner = () => {
+    return (
+        <header>
+            <div>
+                <img src="./globe.svg" alt="logo" />
+            </div>
+            <div>
+            Providing houses all over the world 
+            </div>
+        </header>
+    );
+};
+
+export default Banner;
